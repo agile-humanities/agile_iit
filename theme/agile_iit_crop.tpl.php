3e4bba8c2645474c81a4bce335bbc23da1107821
Rosie Le Faive <lefaive@gmail.com>
2016-04-10 10:22:29 -0300

    Simplify crop callback.

--- theme/agile_iit_crop.tpl.php
+++ theme/agile_iit_crop.tpl.php
@@ -12,11 +12,10 @@
     </div>
     <div style="clear:both;"></div>
     <div id="ol_i1">
-        <img src='<?php echo $img1_src ?>' id='crop_target' width='<?php echo $width ?> 'height='<?php echo $height1 ?>' style=" top:26px; left:0px;"/>
+        <img src='<?php echo $img1_src ?>' id='crop_target'/>
     </div>
     <div id="ol_i2">
-        <img src='<?php echo $img2_src; ?>' width='<?php echo $width ?>' 'height='<?php echo $height2 ?>' style=" top:26px; float:right; left:<?php echo$width / 2 ?>px;"/></a>
-        </a>
+        <img src='<?php echo $img2_src; ?>'/>
     </div>
 </div>
 <div id=results>
