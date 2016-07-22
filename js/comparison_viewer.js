89743ba60a6930728f3208c00c334a1f46a3d7f4
ajstanley <alanjarlathstanley@gmail.com>
2016-07-22 13:16:31 -0300

    fixed resizing issue

--- js/comparison_viewer.js
+++ js/comparison_viewer.js
@@ -5,21 +5,99 @@
  */
 var image1Top, image2Top;
 var image1Left = 0;
-(function ($) {
+function comp_view() {
+    $("#resizable-gallery-wrapper").hide();
+    $("#page-title").hide();
+    var container_width = $('#iit_container').width();
+    var container_height = $('#iit_container').height();
+    var myOverlay = document.createElement('div');
+    myOverlay.id = 'overlay2';
+    var node1 = $("#image1").find('img').data('nid');
+    var node2 = $("#image2").find('img').data('nid');
+    var baseheight = Math.max($("#image1").find('img').height(), $("#image2").find('img').height());
+    $('#iit_container').append(myOverlay);
+    $('#overlay2').width(container_width);
+    $('#overlay2').height(baseheight + 400);
+    var values = [];
+    values.push({name: "node1", 'value': node1});
+    values.push({name: "node2", 'value': node2});
+    $.post("agile/iit/twoviews", values, function (data) {
+        $("#overlay2").append(data);
+        $(window).scrollTop(0);
+        var ol_width = $("#overlay2").width();
+        var ol_height = $("#overlay2").height();
+        // Set image dimensions before calling zoomy.
+        var height = ol_height.toString() + 'px';
+        $("#ol_i1").css("width", ol_width / 2 - 50);
+        $("#ol_i2").css("width", ol_width / 2 - 50); // note: this gets called during window resize.
 
+        var top = Math.max($('#ol_i1').height(), $('#ol_i2').height()) + 5;
+        image1Top = top;
+        image2Top = top;
+        var src1 = $("#ol_i1").find('img').attr('src');
+        var scr2 = $("#ol_i1").find('img').attr('src');
+        $('.zoom').zoomy({
+            zoomSize: 256,
+            round: false,
+            border: '6px solid #fff'
+        });
+        $.ajax({
+            url: 'agile/iit/image_dimensions',
+            type: "POST",
+            data: {
+                img1: $("#ol_i1").find('img').attr('src'),
+                img2: $("#ol_i2").find('img').attr('src'),
+            },
+            async: false,
+            success: function (results, status, xhr) {
+                results = JSON.parse(results)
+                dims = results;
+            },
+            error: function (data, status, xhd) {
+                console.log("The function execute_callback has failed");
+            }
+        });
+
+    });
+}
+function close_compview() {
+    var dims = [];
+    $('.zoom').find('.zoomy').remove();
+    $('#overlay2').remove();
+    $("#resizable-gallery-wrapper").show();
+    $("#page-title").show();
+}
+(function ($) {
     Drupal.behaviors.agileIITComparison = {
         attach: function (context, settings) {
-            $(document).on('click', '#ol_close', function () {
-                var dims = [];
-                $('.zoom').find('.zoomy').remove();
-                $('#overlay2').remove();
-                $("#resizable-gallery-wrapper").show();
-                $("#page-title").show();
+            var rtime;
+            var timeout = false;
+            var delta = 200;
+            $(window).resize(function () {
+                rtime = new Date();
+                if (timeout === false) {
+                    timeout = true;
+                    setTimeout(resizeend, delta);
+                }
             });
+
+            function resizeend() {
+                if (new Date() - rtime < delta) {
+                    setTimeout(resizeend, delta);
+                } else {
+                    timeout = false;
+                    alert('Done resizing');
+                    console.log("resizing");
+                    $("#ol_close").trigger('click');
+                    $("#viewform").trigger('submit');
+                }
+            }
+
+            $(document).on('click', '#ol_close', close_compview);
             $(document).on('click', '#ol_help', function () {
                 var myWindow = window.open('', 'helpWindow', 'width=500, height=500, scrollbars=yes, toolbar=yes');
                 myWindow.focus();
-                $.get("agile/iit/help", "crop", function(data) {
+                $.get("agile/iit/help", "crop", function (data) {
                     myWindow.document.write(data);
                     myWindow.location.href = "#comparison";
                     myWindow.document.close();
@@ -35,65 +113,9 @@ var image1Left = 0;
                     return false;
                 }
                 else {
-                    
-                    $("#resizable-gallery-wrapper").hide();
-                    $("#page-title").hide();
-                    var container_width = $('#iit_container').width();
-                    var container_height = $('#iit_container').height();
-                    var myOverlay = document.createElement('div');
-                    myOverlay.id = 'overlay2';
-                    var node1 = $("#image1").find('img').data('nid');
-                    var node2 = $("#image2").find('img').data('nid');
-                    var baseheight = Math.max($("#image1").find('img').height(), $("#image2").find('img').height());
-                    $('#iit_container').append(myOverlay);
-                    $('#overlay2').width(container_width);
-                    $('#overlay2').height(baseheight + 400);
-                    var values = [];
-                    values.push({name: "node1", 'value': node1});
-                    values.push({name: "node2", 'value': node2});
-                    $.post("agile/iit/twoviews", values, function (data) {
-                        $("#overlay2").append(data);
-                        $(window).scrollTop(0);
-                        var ol_width = $("#overlay2").width();
-                        var ol_height = $("#overlay2").height();
-                        // Set image dimensions before calling zoomy.
-                        var height = ol_height.toString() + 'px';
-                        $("#ol_i1").css("width", ol_width / 2 - 50);
-                        $("#ol_i2").css("width", ol_width / 2 - 50); // note: this gets called during window resize.
-
-                        var top = Math.max($('#ol_i1').height(), $('#ol_i2').height()) + 5;
-                        image1Top = top;
-                        image2Top = top;
-                        var src1 = $("#ol_i1").find('img').attr('src');
-                        var scr2 = $("#ol_i1").find('img').attr('src');
-                        $('.zoom').zoomy({
-                            zoomSize: 256,
-                            round: false,
-                            border: '6px solid #fff'
-                        });
-                        $.ajax({
-                            url: 'agile/iit/image_dimensions',
-                            type: "POST",
-                            data: {
-                                img1: $("#ol_i1").find('img').attr('src'),
-                                img2: $("#ol_i2").find('img').attr('src'),
-                            },
-                            async: false,
-                            success: function (results, status, xhr) {
-                                results = JSON.parse(results)
-                                dims = results;
-                            },
-                            error: function (data, status, xhd) {
-                                console.log("The function execute_callback has failed");
-                            }
-                        });
-
-                    });
-
+                    comp_view();
                 }
-
             });
-
         }
     };
 })(jQuery);
